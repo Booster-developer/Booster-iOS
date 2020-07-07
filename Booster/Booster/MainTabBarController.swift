e165414cc8e1740997154280aea91e6d12c52f17
김태훈 <st939823@gmail.com>
2020-07-08 01:02:12 +0900

    feat:OrderView 다시 구현 TableView로 만들던걸 CollectionView로 다시 만드니 편안해졌다.

--- Booster/Booster/MainTabBarController.swift
+++ Booster/Booster/MainTabBarController.swift
@@ -17,42 +17,43 @@ class MainTabBarController: UITabBarController ,UITabBarControllerDelegate{
         // Do any additional setup after loading the view.
     }
     func setTabBar() {
+      self.tabBar.contentMode = .scaleAspectFill
         
         let homeStoryboard = UIStoryboard.init(name:"Home",bundle: nil)
         guard let firstTab = homeStoryboard.instantiateViewController(identifier: "HomeViewController") as? HomeViewController else{return}
 
         firstTab.tabBarItem.title = ""
-        firstTab.tabBarItem.image = UIImage(named: "btnTab1")?.withRenderingMode(.alwaysOriginal)
-        firstTab.tabBarItem.selectedImage = UIImage(named: "btnTab1")?.withRenderingMode(.alwaysTemplate)
+        firstTab.tabBarItem.image = UIImage(named: "btnTab1Inactive")?.withRenderingMode(.alwaysOriginal)
+      firstTab.tabBarItem.selectedImage = UIImage(named: "btnTab1Active")?.withRenderingMode(.alwaysTemplate)
         
         let searchStoryboard = UIStoryboard.init(name:"Search",bundle: nil)
         guard let secondTab = searchStoryboard.instantiateViewController(identifier: "SearchViewController") as? SearchViewController else{return}
 
         secondTab.tabBarItem.title = ""
-        secondTab.tabBarItem.image = UIImage(named: "btnTab2")?.withRenderingMode(.alwaysOriginal)
-        secondTab.tabBarItem.selectedImage = UIImage(named: "btnTab2")?.withRenderingMode(.alwaysTemplate)
+        secondTab.tabBarItem.image = UIImage(named: "btnTab2Inactive")?.withRenderingMode(.alwaysOriginal)
+        secondTab.tabBarItem.selectedImage = UIImage(named: "btnTab2Active")?.withRenderingMode(.alwaysTemplate)
         
         let orderStoryboard = UIStoryboard.init(name:"Order",bundle: nil)
         guard let thirdTab = orderStoryboard.instantiateViewController(identifier: "OrderViewController") as? UINavigationController else{return}
       
         thirdTab.tabBarItem.title = ""
         thirdTab.tabBarItem.image = UIImage(named: "btnTab3")?.withRenderingMode(.alwaysOriginal)
-        thirdTab.tabBarItem.selectedImage = UIImage(named: "btnTab3")?.withRenderingMode(.alwaysTemplate)
+        //thirdTab.tabBarItem.selectedImage = UIImage(named: "btnTab3Active")?.withRenderingMode(.alwaysTemplate)
         
         let statusStoryboard = UIStoryboard.init(name:"Status",bundle: nil)
         guard let fourthTab = statusStoryboard.instantiateViewController(identifier: "StatusViewController") as? StatusViewController else{return}
         
         fourthTab.tabBarItem.title = ""
-        fourthTab.tabBarItem.image = UIImage(named: "btnTab4")?.withRenderingMode(.alwaysOriginal)
-        fourthTab.tabBarItem.selectedImage = UIImage(named: "btnTab4")?.withRenderingMode(.alwaysTemplate)
+        fourthTab.tabBarItem.image = UIImage(named: "btnTab4Inactive")?.withRenderingMode(.alwaysOriginal)
+        fourthTab.tabBarItem.selectedImage = UIImage(named: "btnTab4Active")?.withRenderingMode(.alwaysTemplate)
         print("kkkk")
 
         let myPageStoryboard = UIStoryboard.init(name:"MyPage",bundle: nil)
         guard let fifthTab = myPageStoryboard.instantiateViewController(identifier: "MyPageViewController") as? MyPageViewController else{return}
 
         fifthTab.tabBarItem.title = ""
-        fifthTab.tabBarItem.image = UIImage(named: "btnTab5")?.withRenderingMode(.alwaysOriginal)
-        fifthTab.tabBarItem.selectedImage = UIImage(named: "btnTab5")?.withRenderingMode(.alwaysTemplate)
+        fifthTab.tabBarItem.image = UIImage(named: "btnTab5Inactive")?.withRenderingMode(.alwaysOriginal)
+        fifthTab.tabBarItem.selectedImage = UIImage(named: "btnTab5Active")?.withRenderingMode(.alwaysTemplate)
 
         let tabs = [firstTab, secondTab, thirdTab,fourthTab,fifthTab]
         self.setViewControllers(tabs, animated: false)
